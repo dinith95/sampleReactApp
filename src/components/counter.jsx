e3cde50dc843616f15edfd9b68793f5d18732241
dinith72 <jdinith72@gmail.com>
2019-02-07 23:08:50 +0530

    started single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  state = {
-    count: this.props.value, // declare the count as props value
-    tags: [
-      'tag 01' , 'tag 02' , 'tag 03'
-    ]
-  };
+  // state = {
+  //   count: this.props.value, // declare the count as props value
+  //   tags: ["tag 01", "tag 02", "tag 03"]
+  // };
 
   styles = {
     // fontSize: 20,
     fontWeight: "bold"
   };
-  // get count method => return number if count > 1 else return zero 
+  // get count method => return number if count > 1 else return zero
   getCount() {
-    return this.state.count === 0 ? <h1>Zero</h1> : this.state.count; // jsx can be directly returned 
+    return this.state.count === 0 ? <h1>Zero</h1> : this.state.count; // jsx can be directly returned
   }
-  // conditional style classes 
+  // conditional style classes
   getClasses() {
     let styleString = "badge m-2 ";
     if (this.state.count === 0) {
@@ -25,41 +23,46 @@ class Counter extends Component {
     }
     return styleString;
   }
-  getTags(){
-    if(this.state.tags.length === 0){
-      return <p> no elements avalable </p>
-    }else {
-      return this.state.tags.map(tag => <li key = {tag}>{ tag }</li> )
-    }
-  }
 
-    handleIncrement = () => {
-      // state should be updated 
+  handleIncrement = () => {
+    // state should be updated
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  handleDelete = () => {
+    if (this.state.count > 0) {
       this.setState({
-        count: this.state.count + 1 ,
-      })
+        count: this.state.count - 1
+      });
     }
+  };
 
-    handleDelete = () =>  {
-      if(this.state.count > 0){
-        this.setState({
-          count: this.state.count - 1
-        });
-      }
-      
-    }
-  
   render() {
     return (
-      
-      <React.Fragment> 
+      <React.Fragment>
         {this.props.children}
-        <span className={this.getClasses()}>{this.getCount()} </span> 
-       
+        <span className={this.getClasses()}>{this.getCount()} </span>
 
-        <button onClick={ () => this.handleIncrement()} className="btn btn-secondary btn-sm">increment</button>
-        <button onClick= {() => this.handleDelete()} className="btn-warning m-2"> Decrement</button>
-        <button onClick = {() => this.props.onDelete(this.props.id)} className ="btn-danger"> Delete</button>
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-secondary btn-sm"
+        >
+          increment
+        </button>
+        <button onClick={() => this.handleDelete()} className="btn-warning m-2">
+          {" "}
+          Decrement
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn-danger"
+        >
+          {" "}
+          Delete
+        </button>
+
         <img src={this.state.image} alt="" />
         {/* <ul>
           {this.getTags()}
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,50 +1,64 @@
-import React, { Component } from 'react';
-import Counter from './counter';
+import React, { Component } from "react";
+import Counter from "./counter";
 
 class Counters extends Component {
-    state = { 
-        counters : [
-            {id: 1 , value: 4 },
-            {id: 2 , value: 0 },
-            {id: 3 , value: 0 },
-            {id: 4 , value: 0 },
-            {id: 5 , value: 5},
-        ]      
-    }
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+      { id: 5, value: 5 }
+    ]
+  };
 
-    handleDelete = (id) => {
-        // console.log('delete clicked ', id);
-        const counters = this.state.counters.filter(counter => {
-            if(counter.id != id){
-                return counter;
-            }
-        });
-        // console.log('counters :', counters);
-        this.setState({
-            counters: counters
-        })
-    }
-     
-    render() { 
-        return ( 
-            <React.Fragment>
-                <ul>
-                {this.state.counters.map( 
-                    counter => 
-                    <Counter 
-                        id = {counter.id}
-                        key={counter.id} 
-                        value = {counter.value}  
-                        onDelete = { this.handleDelete} > 
-                            <h2> counter  {counter.id}</h2>
-                    </Counter>)}
-                            {/* add counter id and the value to the list of counter tags  */}
-                </ul>
-                    
-                
-            </React.Fragment>
-         );
-    }
+  handleDelete = id => {
+    // console.log('delete clicked ', id);
+    const counters = this.state.counters.filter(counter => {
+      if (counter.id != id) {
+        return counter;
+      }
+    });
+    // console.log('counters :', counters);
+    this.setState({
+      counters: counters
+    });
+  };
+
+  handleReset = () => {
+    console.log("reset handle");
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({
+      counters: counters
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.handleReset} className="btn btn-primary">
+          {" "}
+          REset{" "}
+        </button>
+        <ul>
+          {this.state.counters.map(counter => (
+            <Counter
+              id={counter.id}
+              key={counter.id}
+              value={counter.value}
+              onDelete={this.handleDelete}
+            >
+              <h2> counter {counter.id}</h2>
+            </Counter>
+          ))}
+          {/* add counter id and the value to the list of counter tags  */}
+        </ul>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Counters;
